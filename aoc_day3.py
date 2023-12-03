d42f3833fad8635efeddefe7f273b52c3333c05b
Brownie2002 <brownie2002@gmail.com>
2023-12-03 14:43:06 +0100

    Day3 part1 - solved

--- aoc_day3.py
+++ aoc_day3.py
@@ -1,9 +1,50 @@
 import logging
 import sys
+import pandas as pd
 
 def aoc_day3( file ):
 
+    data = pd.read_csv(file, header=None)
+    df = data[0].apply(lambda x: pd.Series(list(x)))
+
+    symbols="*#+$/=%&"
+    symbols_dict={}
+    number=""
+    is_part_number = False
+    part_list=[]
+
+    for y in range(len(df.columns)):
+        for x in range(len(df)):
+            if not df[x][y].isnumeric() and not df[x][y] == ".":
+                symbols_dict[df[x][y]] = True
+
+    for y in range(len(df)):
+        for x in range(len(df.columns)):
+            if df[x][y].isnumeric():
+                number = number + df[x][y]
+                for i in [-1,0,1]:
+                    for j in [-1,0,1]:
+                        try:
+                            if df[x+i][y+j] in symbols_dict:
+                                is_part_number = True
+                                continue
+                        except KeyError:
+                            continue
+                try:
+                    next_character = df[x+1][y]
+                except KeyError:
+                    next_character = "."
+                if not next_character.isnumeric():
+                    # We reach the end of a number
+                    if number and is_part_number:
+                        part_list.append(number)
+                    number=""
+                    is_part_number=False
     part1 = 0
+    part_list_wo_dup = list(dict.fromkeys(part_list))
+    for value in part_list:
+        # Duplicate parts are removed
+        part1 = part1 + int(value)
     part2 = 0
 
     return part1, part2
@@ -11,7 +52,7 @@ def aoc_day3( file ):
 def main() -> int:
     # --- Day 3: Gear Ratios ---
     value_part1, value_part2 = aoc_day3("inputs/day3.input")
-    print(f"Day 3: part 1 is {value_part1} and part 2 is {value_part1}.")
+    print(f"Day 3: part 1 is {value_part1} and part 2 is {value_part2}.")
 
     return 0
 
--- main_test.py
+++ main_test.py
@@ -1,6 +1,7 @@
 from aoc_day3 import aoc_day3
 from main import aoc_day1, aoc_day2
 
+
 def test_aoc_day1():
     part1, part2 = aoc_day1("inputs/day1_test_part1.input")
     assert part1 == 142
@@ -23,9 +24,17 @@ def test_aoc_day2():
     assert power == 72706
 
 def test_aoc_day3():
-    part1, part2 = aoc_day3("inputs/day3_test.input")
+    part1, part2 = aoc_day3("inputs/day3_test1.input")
     assert part1 == 4361
     assert part2 == 0
+    # From https://www.reddit.com/r/adventofcode/comments/189q9wv/2023_day_3_another_sample_grid_to_use/
+    part1, part2 = aoc_day3("inputs/day3_test2.input")
+    assert part1 == 925
+    assert part2 == 6756
+    part1, part2 = aoc_day3("inputs/day3_test3.input")
+    assert part1 == 413
+    assert part2 == 6756
+
     # My data
     part1, part2 = aoc_day3("inputs/day3.input")
     assert part1 == 0
