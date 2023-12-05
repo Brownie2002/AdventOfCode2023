ad633f750ae57117bb4b09f60f9d4a017d4df6df
Bertrand de Saint-Jean <bertrand.desaint-jean@airbus.com>
2023-12-05 14:30:41 +0000

    Solution try1 for day 5

--- day5/main.py
+++ day5/main.py
@@ -1,9 +1,21 @@
 import logging
 import os
 import sys
-import numpy as np
+from time import perf_counter
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+def compute_target_number( source, rules ):
+
+    target=source
+
+    # Check if the source is in a range of the rules
+    for rule in rules:
+        if source >= rule[1] and source < (rule[1] + rule[2]):
+            # I am in the correct range
+            target = source + ( rule[0] - rule[1])
+
+    return target
 
 def solve( file ):
 
@@ -13,44 +25,82 @@ def solve( file ):
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, file)
 
-    score=0
+    seeds=[]
+    maps={}
+    targets={}
 
-    logging.info(f"Input full path: {filename}")
-
-    num_original_cards=0
-    with open(filename, "rb") as f:
-        num_original_cards = sum(1 for _ in f)
-
-    nb_cards=np.full(num_original_cards, 1)
-
-    with open(filename) as f:
+    with open(filename, "r") as f:
         for line in f:
-            nb_match = 0
-            info=line.rstrip('\n').split(":")
-            card_number=int(info[0].split()[1])
-            winning, having=info[1].strip().split("|")
-            having=having.split()
-            for win_number in winning.split():
-                if win_number in having:
-                    nb_match += 1
-            for new_card in range(nb_match):
-                logging.debug(f"with card {card_number} win card {new_card + card_number + 1}")
-                nb_cards[new_card + card_number] +=  nb_cards[card_number - 1]
-            if nb_match > 0:
-                score = score + 2**(nb_match-1)
+            if "seeds:" in line:
+                seeds=[int(x) for x in line.split()[1:]]
+                continue
+            if " map:" in line:
+                map=line.split()[0]
+                current_map=map
+                maps[current_map]={}
+                maps[current_map]['rules']=[]
+                source, target= map.split("-to-")
+                targets[source]=target
+                continue
+            if not line.rstrip():
+                # Skip blank line
+                continue
+            
+            # We are on a line with numbers "dst_start, src_start, nb"
+            maps[current_map]['rules'].append([int(x) for x in line.split()])
 
-    part1=score
-    part2=np.sum(nb_cards)  
-    # Find card separation column
+    
+    part1=1e12
+
+    total_of_seeds=0
+    for seed_index in range(0,len(seeds),2):
+        total_of_seeds += seeds[seed_index+1]
+    print(f"Number of seeds : {total_of_seeds}")
+    fraction_01=int(total_of_seeds/10000)
+
+    # All the maps are done, compute way
+    index=0
+    for seed_index in range(0,len(seeds),2):
+        for seed in range(seeds[seed_index],seeds[seed_index]+seeds[seed_index+1]):
+            index += 1
+            if (index % fraction_01) == 1:
+                print(f"Reach over {total_of_seeds} reached {index*100/total_of_seeds:.2f}%, index {index}.")
+            source_category="seed"
+            source_number=seed
+            seeds_dict={}
+#            print(f"{source_category} {source_number}, ", end="")
+            while True:
+                # Select the map
+                try:
+                    target_category=targets[source_category]
+                except:
+                    break
+                map=source_category+"-to-"+target_category
+
+                target_number=compute_target_number(source_number, maps[map]['rules'])
+
+#                print(f"{target_category} {target_number}, ", end="")
+                seeds_dict[target_category]=target_number
+
+                # Prepare next target
+                source_number=target_number
+                source_category=target_category
+#            print('')
+            if seeds_dict["location"] < part1:
+                part1=seeds_dict["location"]
+
+                print(f"Location is {part1}")
 
     return part1, part2
 
 def main() -> int:
     # --- Day 5: If You Give A Seed A Fertilizer ---
     # https://adventofcode.com/2023/day/5
-    value_part1, value_part2 = solve( "inputs/test1.input" )
+    t1_start = perf_counter()
+    value_part1, value_part2 = solve( "inputs/personnal.input" )
+    t1_stop = perf_counter()
     print(f"Day 5: part 1 is {value_part1} and part 2 is {value_part2}.")
-
+    print(f"Elapsed time(s): {t1_stop-t1_start}")
     return 0
 
 if __name__ == '__main__':
--- day5/main_test.py
+++ day5/main_test.py
@@ -3,9 +3,9 @@ from main import solve
 def test_example():
     part1, part2 = solve("inputs/test1.input")
     assert part1 == 35
-    assert part2 == 1
+    assert part2 == 46
 
 def test_my_data():
     part1, part2 = solve("inputs/personnal.input")
-    assert part1 == 0
+    assert part1 == 178159714
     assert part2 == 1
